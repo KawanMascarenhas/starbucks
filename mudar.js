236981746f554839b309dabed4b36fc431838d76
KawanMascarenhas <kawanmascarenhas@gmail.com>
2023-04-20 15:53:16 -0300

    Finalizado

---
+++ mudar.js
@@ -0,0 +1,14 @@
+function imgSlider(anything){
+    document.querySelector('.starbucks').src=anything;
+}
+function changeCircleColor(color){
+const circulo =document.querySelector('.circulo');
+circulo.style.background=color;
+}
+function toggleMenu(){
+var menuToogle = document.querySelector('toggle');
+varlinks = document.querySelector('.links');
+menuToogle.classList.toggle('active')
+links.classlist.toggle('active')
+
+}
